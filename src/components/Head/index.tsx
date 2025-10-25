e4e78a3b01e4b94f933182b5adebf209eae863d7
Kiwi-Eric-Li <lxf7756@126.com>
2025-10-25 17:15:16 +0800

    modify head style

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -7,8 +7,10 @@ function Head() {
     return (
         <div className="header">
             <Decoration8 style={{width: '300px', 'height': '70px'}}/>
-            <div className='head-title'>Large-screen Project</div>
-            <Decoration5 style={{width: '300px', height: '90px'}}/>
+            <div className='head-title'>
+                <span>Large-screen Project</span>
+                <Decoration5 style={{width: '600px', height: '90px'}}/>
+            </div>
             <Decoration8 reverse={true} style={{width: '300px', 'height': '70px'}}/>
         </div>
     )
