bc241d659f52a678c0bdf8d0effbe95a9363c8b5
Kiwi-Eric-Li <lxf7756@126.com>
2025-10-28 10:35:37 +0800

    add a deployment package

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      deleteOriginFile: false,
+      threshold: 10240, // 体积大于10k才进行压缩
+      algorithm: 'gzip',  // 使用算法
+      ext: '.gz'  // 生成的文件名后缀
+    })
+  ],
   build: {
     rollupOptions: {
       output: {
